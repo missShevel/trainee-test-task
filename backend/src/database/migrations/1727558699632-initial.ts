f71bc61117eb4cb9bdcb2e131701399daae0d6e0
missShevel <o.v.shevel@gmail.com>
2024-09-29 00:27:59 +0300

    fix migration

---
+++ backend/src/database/migrations/1727558699632-initial.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Initial1727558699632 implements MigrationInterface {
+    name = 'Initial1727558699632'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TYPE "public"."cards_status_enum" AS ENUM('todo', 'inProgress', 'done')`);
+        await queryRunner.query(`CREATE TABLE "cards" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "title" character varying NOT NULL, "description" character varying, "status" "public"."cards_status_enum" NOT NULL DEFAULT 'todo', "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "boardId" uuid, CONSTRAINT "PK_5f3269634705fdff4a9935860fc" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "boards" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_606923b0b068ef262dfdcd18f44" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`ALTER TABLE "cards" ADD CONSTRAINT "FK_a45a6573ee229a27dd6d15bf11c" FOREIGN KEY ("boardId") REFERENCES "boards"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "cards" DROP CONSTRAINT "FK_a45a6573ee229a27dd6d15bf11c"`);
+        await queryRunner.query(`DROP TABLE "boards"`);
+        await queryRunner.query(`DROP TABLE "cards"`);
+        await queryRunner.query(`DROP TYPE "public"."cards_status_enum"`);
+    }
+
+}
