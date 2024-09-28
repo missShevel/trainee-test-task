e1d881427ddea88d3db3ccda3f497652a28e0751
missShevel <o.v.shevel@gmail.com>
2024-09-28 23:35:57 +0300

    run linters on backend

--- backend/src/board/board.controller.ts
+++ backend/src/board/board.controller.ts
@@ -3,7 +3,6 @@ import {
   Controller,
   Delete,
   Get,
-  NotFoundException,
   Param,
   ParseUUIDPipe,
   Post,
--- backend/src/card/dto/createCard.dto.ts
+++ backend/src/card/dto/createCard.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsOptional, MaxLength } from 'class-validator';
+import { IsOptional, MaxLength } from 'class-validator';
 
 export class CreateCardDto {
   @MaxLength(255)
